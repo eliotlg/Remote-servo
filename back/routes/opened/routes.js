52c8f71debfdecaab4bc272f2aebb40e744548c0
Eliot <eliot.le-gall@epitech.eu>
2021-03-01 01:28:06 +0100

    [Add]: Embedded part is working fine, job done

--- back/routes/opened/routes.js
+++ back/routes/opened/routes.js
@@ -1,33 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../../controllers/product');
-const Login = require('../../controllers/login');
-const Key = require('../../controllers/key');
-const multer = require('multer');
-var randomstring = require("randomstring");
 var path = require('path');
 var cors = require('cors');
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-        console.log(file);
-        cb(null, randomstring.generate(9) + new Date().getMilliseconds() + path.extname(file.originalname));
-    }
-});
-const upload = multer({storage: storage});
 
 
 var corsOptions = {
     origin: '*'
 }
 
-var cyclic = "2";
+var cyclic = 2;
 
 router.get("/set/:cycle", cors(corsOptions), async(req, res) => {
     try {
-        cyclic = req.params.cycle;
+        cyclic = Number(req.params.cycle);
         res.json(req.params.cycle);
     } catch (err) {
         console.error("error: ", err);
--- embedded/pwm.c
+++ embedded/pwm.c
@@ -3,20 +3,40 @@
 #include <unistd.h>
 #include <gpiod.h>
 #include <stdbool.h>
+#include <curl/curl.h>
+#include <string.h>
+
+
+int cyclic = 2;
+
+void function_pt(void *ptr, size_t size, size_t nmemb, void *stream){
+    char *tmp = malloc(sizeof(char) * (nmemb + 1));
+    memset(tmp, 0, sizeof(char) * (nmemb + 1));
+    for (int i = 0; i < nmemb; i++)
+      tmp[i] = ((char *)ptr)[i];
+    cyclic = atoi(tmp);
+    fprintf(stdout, "cyclic: %d\n", cyclic);
+}
+
+void call_get(void)
+{
+  CURL *curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000/get");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, function_pt);
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+  }
+}
 
 int main(int ac, char **av)
 {
   struct gpiod_chip *chip = gpiod_chip_open_by_name("gpiochip0");
   struct gpiod_line *line = gpiod_chip_get_line(chip, 17);
   int ret = gpiod_line_request_output(line, "Consumer", 0);
-  fprintf(stdout, "Enter cyclic\n", ret);
-  char buffer[80];
-  int cyclic;
 
-  scanf("%[^\n]", buffer);
-  fgetc(stdin);
-  fprintf(stdout, "Thanks!\n");
-  cyclic = atoi(buffer);
+  call_get();
+
 
   ret = gpiod_line_set_value(line, false);
 
@@ -24,14 +44,21 @@ int main(int ac, char **av)
   int implicit_cyclic_on = (implicit_cyclic * cyclic) / 100;
   int implicit_cyclic_off = implicit_cyclic - implicit_cyclic_on;
 
+  int tmp = 0;
+
   fprintf(stdout, "Starting motor control with a cyclic of %d\n", cyclic);
   while (true) {
+    if (tmp >= 100) {
+      call_get();
+      implicit_cyclic_on = (implicit_cyclic * cyclic) / 100;
+      implicit_cyclic_off = implicit_cyclic - implicit_cyclic_on;
+      tmp = 0;
+    }
     ret = gpiod_line_set_value(line, true);
-    //    fprintf(stdout, "%d\n", ret);
     usleep(implicit_cyclic_on);
     ret = gpiod_line_set_value(line, false);
-    //    fprintf(stdout, "%d\n", ret);
     usleep(implicit_cyclic_off);
+    tmp += 1;
   }
   return (0);
 }
